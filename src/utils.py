a25781aafe270d5d322b8f962e1b9e65d53de285
Homero Esmeraldo <homero.esmeraldo@gmail.com>
2021-03-19 16:20:08 -0300

    Fix bug in function that creates lists of waterport visits times, extend it and create more tests

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,11 @@ import numpy as np
 
 
 def get_wp_visit_times_and_rwd_times(tf):
+    """
+    Get lists of waterport visit times and reward delivery times
+    :param tf: trajectory object
+    :return: times_to_waterport_visits, times_to_rwd
+    """
     frs_to_waterport_visits = []
     n_bouts = len(tf.no)
     for bout in range(n_bouts):
@@ -25,32 +30,38 @@ def get_wp_visit_times_and_rwd_times(tf):
     return times_to_waterport_visits, times_to_rwd
 
 
-def create_list_waterport_visits_in_between_rwds(times_to_waterport_visits, times_to_rwd):
-    """ Creates list of all waterport visits in between each of the rewards in `times_to_rwd`
+def create_list_waterport_visits_in_between_rwds(times_to_waterport_visits, times_to_rwd, include_wp_visits_after_last_rwd=False):
+    """ Creates list of all waterport visits in between each of the rewards in `times_to_rwd`. Includes visits before
+    first reward, but does not include visits after the last reward.
     :param times_to_waterport_visits: list of times of visits to the waterport node
     :param times_to_rwd: list of times of reward delivery
+    :param include_wp_visits_after_last_rwd: Use True to include waterport visits after last reward delivery. Default is False.
     :return: a list of lists. Each list has the waterport visits in between rewards. The first element has the visits
-    before any reward delivery
+    before any reward delivery.
     """
+    # TODO: this function would probably have been more readable if I had done an external loop of the rwd visits instead of waterport visits.
+    #  Maybe change someday. But seems to be working now as it is. Good luck understanding it! ;)
     all_waterport_visits = []
     waterport_visits_in_between_rwds = []
     rwd_i = 0
     for wp_idx, waterport_visit_time in enumerate(times_to_waterport_visits):  # create list of all waterport visits in between rewards
         # print('wp ', waterport_visit_time)
-        if rwd_i < len(times_to_rwd):
+        if rwd_i < len(times_to_rwd):  # if there are still reward deliveries after current waterport visit
             if waterport_visit_time < times_to_rwd[rwd_i]:  # get waterport visits before each rwd delivery
                 waterport_visits_in_between_rwds.append(waterport_visit_time)
+                if wp_idx == len(times_to_waterport_visits) - 1:  # if it is the last item in the list
+                    # print('wp ', waterport_visits_in_between_rwds)
+                    all_waterport_visits.append(waterport_visits_in_between_rwds)
             else:  # reached waterport visit that is already after currently considered rwd delivery
-                # print('wp ', waterport_visits_in_between_rwds)
                 # print('rwd ', times_to_rwd[rwd_i])
+                # print('wp ', waterport_visits_in_between_rwds)
                 rwd_i += 1
                 all_waterport_visits.append(waterport_visits_in_between_rwds)
                 waterport_visits_in_between_rwds = []
-                waterport_visits_in_between_rwds.append(waterport_visit_time)  # add to the list of visits after next rwd
-        else:
+                waterport_visits_in_between_rwds.append(waterport_visit_time)  # add current waterport visit to the list of visits after next rwd
+        elif include_wp_visits_after_last_rwd:  # waterport visits after last reward visit
             waterport_visits_in_between_rwds.append(waterport_visit_time)  # add to the list of visits after the last rwd
-            if wp_idx == len(times_to_waterport_visits)-1:
-                # print("end")
+            if wp_idx == len(times_to_waterport_visits)-1:  # if it is the last item in the list
                 # print('wp ', waterport_visits_in_between_rwds)
                 all_waterport_visits.append(waterport_visits_in_between_rwds)
 
--- tests/test_utils_functions.py
+++ tests/test_utils_functions.py
@@ -4,9 +4,32 @@ waterport_visits = [1, 5, 7, 8, 10, 11]
 rwd_visits = [2, 6, 9]
 
 answer = create_list_waterport_visits_in_between_rwds(waterport_visits, rwd_visits)
-ref_answer = [[1], [5], [7, 8], [10, 11]]
+ref_answer = [[1], [5], [7, 8]]  # note that it does not include the visits after the last reward
 
 print(answer)
 
 assert  answer == ref_answer, \
-    "Error! Waterport visits in between reward deliveries do not match the reference answer"
+    "Error! Waterport visits in between reward deliveries %s do not match the reference answer %s" % (answer, ref_answer)
+
+waterport_visits = [1, 5, 7, 8]
+rwd_visits = [2, 6, 9]
+
+answer = create_list_waterport_visits_in_between_rwds(waterport_visits, rwd_visits)
+ref_answer = [[1], [5], [7, 8]]
+
+print(answer)
+
+assert  answer == ref_answer, \
+    "Error! Waterport visits in between reward deliveries %s do not match the reference answer %s when there are " \
+    "no waterport visits after the last reward" % (answer, ref_answer)
+
+waterport_visits = [1, 5, 7, 8, 10, 11]
+rwd_visits = [2, 6, 9]
+
+answer = create_list_waterport_visits_in_between_rwds(waterport_visits, rwd_visits, include_wp_visits_after_last_rwd=True)
+ref_answer = [[1], [5], [7, 8], [10, 11]]  # note that it does not include the visits after the last reward
+
+print(answer)
+
+assert  answer == ref_answer, \
+    "Error! Waterport visits in between reward deliveries %s do not match the reference answer %s" % (answer, ref_answer)
