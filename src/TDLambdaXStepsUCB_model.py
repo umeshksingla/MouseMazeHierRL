6b34256266f2a69ff6434fc5993df094da5772d4
Shuangquan Feng <s1feng@ucsd.edu>
2021-06-25 12:50:41 -0700

    Change the UCB model from episodic to continual

--- src/TDLambdaXStepsUCB_model.py
+++ src/TDLambdaXStepsUCB_model.py
@@ -16,14 +16,27 @@ from MM_Traj_Utils import *
 
 class TDLambdaXStepsUCBRewardReceived(TDLambdaXStepsRewardReceived):
     
+    def __init__(self, X = 20, file_suffix='_XStepsRewardReceivedTrajectories'):
+        TDLambdaXStepsRewardReceived.__init__(self, file_suffix)
+        self.terminal_nodes = {HomeNode}
+    
     def get_initial_state(self):
         return 0
     
-    def generate_episode(self, alpha, beta, gamma, lamda, MAX_LENGTH, V, e, c, t, N):
+    #def get_action_probabilities(self, state, beta, V):
+    #    if state == HomeNode:
+    #        action_prob = [0, 1, 0]
+    #    else:
+    #        action_prob = TDLambdaXStepsRewardReceived.get_action_probabilities(self, state, beta, V)
+    #    return action_prob
+    
+    def generate_episode(self, alpha, beta, gamma, lamda, V, e, c, t, N, TimeEachMove=1):
 
         s = self.get_initial_state()
         episode_traj = []
-        valid_episode = False
+        episode_traj_wo_rew = []
+        #valid_episode = False
+        TimeFromLastReward = 90
         
         #start from the home node
         N[HomeNode] += 1
@@ -31,17 +44,26 @@ class TDLambdaXStepsUCBRewardReceived(TDLambdaXStepsRewardReceived):
         while s not in self.terminal_nodes:
 
             episode_traj.append(s)  # Record current state
-
-            if s != RewardNode:
+            if s != WaterPortNode:
+                episode_traj_wo_rew.append(s)#Do not Record the Water Port Node
+            
+            if s == WaterPortNode:
+                s_next = RewardNode
+                R = 1
+                TimeFromLastReward = 0
+            elif (s == RewardNode) & (TimeFromLastReward >= 90):
+                s_next = WaterPortNode
+                R = 0
+            else:
                 #print(c * np.sqrt(np.log(t)/N))
                 action_prob = self.get_action_probabilities(s, beta, V + c * np.sqrt(np.log(t)/N))
                 a = np.random.choice(range(self.A), 1, p=action_prob)[0]  # Choose action
                 s_next = int(self.nodemap[s, a])           # Take action
                 # print("s, s_next, a, action_prob", s, s_next, a, action_prob)
-            else:
-                s_next = WaterPortNode
+                TimeFromLastReward += TimeEachMove #time
+                R = 0
 
-            R = 1 if s == RewardNode else 0  # Observe reward
+            #R = 1 if s == RewardNode else 0  # Observe reward
 
             # Update state-values
             td_error = R + gamma * V[s_next] - V[s]
@@ -52,6 +74,7 @@ class TDLambdaXStepsUCBRewardReceived(TDLambdaXStepsRewardReceived):
                 V[node] += alpha * td_error * e[node]
                 e[node] = gamma * lamda * e[node]
 
+            #print('Water Port Node: ' + str(V[WaterPortNode]))
             # print("V[s]", s, V[s])
             if np.isnan(V[s]):
                 print('Warning invalid state-value: ', s, s_next, V[s], V[s_next], alpha, beta, gamma, R)
@@ -61,29 +84,31 @@ class TDLambdaXStepsUCBRewardReceived(TDLambdaXStepsRewardReceived):
                 print('Warning state value exceeded upper bound. Might approach infinity.')
                 V[s] = np.sign(V[s]) * 1e5
 
-            if s == RewardNode:
-                print('Reward Reached. Recording episode.')
-                valid_episode = True
-                break
+            if s == WaterPortNode:
+                print('Reward Reached')
+            #if s == RewardNode:
+            #    print('Reward Reached. Recording episode.')
+            #    valid_episode = True
+            #    break
 
-            if len(episode_traj) > MAX_LENGTH:
-                print('Trajectory too long. Aborting episode.')
-                valid_episode = False
-                break
+            #if len(episode_traj) > MAX_LENGTH:
+            #    print('Trajectory too long. Aborting episode.')
+            #    valid_episode = False
+            #    break
 
             s = s_next
             
-        if s == HomeNode:
-            episode_traj.append(s)
+        #if s == HomeNode:
+        #    episode_traj.append(s)
         
-        e[s] += 1
+        #e[s] += 1
         N[s] += 1
 
-        return valid_episode, [episode_traj]
+        return [episode_traj], [episode_traj_wo_rew]
     
     
     
-    def simulate(self, sub_fits, MAX_LENGTH=25, N_BOUTS_TO_GENERATE=100):
+    def simulate(self, sub_fits, N_MOVES = 10000):#:MAX_LENGTH=25, N_BOUTS_TO_GENERATE=100):
         """
         Model predictions (sample predicted trajectories) using fitted parameters sub_fits.
         You can use this to generate simulated data for parameter recovery as well.
@@ -100,21 +125,17 @@ class TDLambdaXStepsUCBRewardReceived(TDLambdaXStepsRewardReceived):
         episodes_all_mice:
             dictionary of trajectories simulated by a model using fitted 
             parameters for specified mice. e.g. {0:[0,1,3..], 1:[]..}
-        invalid_episodes_all_mice:
-            dictionary of trajectories simulated by a model using fitted
-            parameters for specified mice. e.g. {0:[0,1,3..], 1:[]..}
-            where they didn't reach the reward in even MAX_LENGTH steps
-        success:
-            int. either 0 or 1 to flag when the model fails to generate
-            trajectories adhering to certain bounds: fitted parameters, 
-            number of episodes, trajectory length, etc.
+        episodes_all_mice_wo_rew:
+            is similar to episodes_all_mice but excludes the reward nodes in order for
+            plotting
+
         stats:
             dict. some stats on generated traj
         """
 
         stats = {}
         episodes_all_mice = defaultdict(dict)
-        invalid_episodes_all_mice = defaultdict(dict)
+        episodes_all_mice_wo_rew = defaultdict(dict)
         episode_cap = 500   # max attempts at generating a bout episode
         success = 1
 
@@ -125,9 +146,13 @@ class TDLambdaXStepsUCBRewardReceived(TDLambdaXStepsRewardReceived):
             gamma = sub_fits[mouseID][2]    # discount factor
             lamda = sub_fits[mouseID][3]    # eligibility trace
             c = sub_fits[mouseID][4]        # degree of exploration
+            if len(sub_fits[mouseID]) > 5:
+                TimeEachMove = sub_fits[mouseID][5] 
+            else:
+                TimeEachMove = 1
 
-            print("alpha, beta, gamma, lamda, mouseID, c, nick",
-                  alpha, beta, gamma, lamda, mouseID, c, RewNames[mouseID])
+            print("alpha, beta, gamma, lamda, mouseID, c, speed, nick",
+                  alpha, beta, gamma, lamda, mouseID, c, TimeEachMove, RewNames[mouseID])
 
             V = np.random.rand(self.S+1)  # Initialize state-action values
             #V = np.zeros(self.S+1)
@@ -140,53 +165,24 @@ class TDLambdaXStepsUCBRewardReceived(TDLambdaXStepsRewardReceived):
             t = np.ones(1)
             N = np.ones(self.S+1)
 
-            valid_index = []
             episodes = []
-            invalid_episodes = []
-            count_valid, count_total = 0, 1
-            while len(episodes) < N_BOUTS_TO_GENERATE:
+            episodes_wo_rew = []
+            n_bouts = 0
 
-                # Back-up a copy of state-values to use in case the next episode has to be discarded
-                V_backup = np.copy(V)
-                e_backup = np.copy(e)
-                t_backup = np.copy(t)
-                N_backup = np.copy(N)
+            while t <= N_MOVES:
+                episode_traj, episode_traj_wo_rew = self.generate_episode(alpha, beta, gamma, lamda, V, e, c, t, N, TimeEachMove)
+                n_bouts += 1
+                episodes.extend(episode_traj)
+                episodes_wo_rew.extend(episode_traj_wo_rew)
 
-                # Begin generating episode
-                episode_attempt = 0
-                valid_episode = False
-                while not valid_episode and episode_attempt <= episode_cap:
-                    episode_attempt += 1
-                    valid_episode, episode_traj = self.generate_episode(alpha, beta, gamma, lamda, MAX_LENGTH, V, e, c, t, N)
-                    count_valid += int(valid_episode)
-                    count_total += 1
-                    if valid_episode:
-                        episodes.extend(episode_traj)
-                        valid_index.append(1)
-                    else:   # retry
-                        #V = np.copy(V_backup)   # TODO: maybe not discard invalid trajs?
-                        #e = np.copy(e_backup)
-                        #t = np.copy(t_backup)
-                        #N = np.copy(N_backup)
-                        invalid_episodes.extend(episode_traj)
-                        valid_index.append(0)
-                    print("===")
-                if not count_valid:
-                    print('Failed to generate episodes for mouse ', mouseID)
-                    success = 0
-                    break
-                # print("=============")
             episodes_all_mice[mouseID] = episodes
-            invalid_episodes_all_mice[mouseID] = invalid_episodes
+            episodes_all_mice_wo_rew[mouseID] = episodes_wo_rew
             stats[mouseID] = {
                 "mouse": RewNames[mouseID],
-                "MAX_LENGTH": MAX_LENGTH,
-                "count_valid": count_valid,
-                "count_total": count_total,
-                "fraction_valid": round(count_valid/count_total, 3) * 100,
-                "valid_index": np.array(valid_index),
-                "V": V.round(4),
-                "N": N.round(4)
+                "n_moves": t,
+                "n_bouts": n_bouts,
+                "state_values": V.round(4),
+                "visit_frequency": N.round(4)
                 # "invalid_initial_state_counts": invalid_initial_state_counts
             }
-        return episodes_all_mice, invalid_episodes_all_mice, success, stats
+        return episodes_all_mice, episodes_all_mice_wo_rew, stats
