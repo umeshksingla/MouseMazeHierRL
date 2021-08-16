b6923fc4ba8d406b383dfb2423e92cdc4fc71164
Umesh Singla <umeshksingla@gmail.com>
2021-08-16 14:18:58 -0700

    update return format for TDLambdaOptInit model

--- src/TDLambdaOptimisticInitialization.py
+++ src/TDLambdaOptimisticInitialization.py
@@ -25,32 +25,31 @@ class TDLambdaOptimisticInitialization(DynaQPlus):
         epsilon = params["epsilon"]     # epsilon
         k = 0                           # bonus factor
         n_plan = 0                      # number of planning steps
-        self.back_action = True         # if there is a back action
+        bonus_in_planning = False       # check dynaQ+ for this
 
         print("alpha, gamma, lamda, epsilon, k, n_plan, back_action, agentId",
               alpha, gamma, lamda, epsilon, k, n_plan, self.back_action, agentId)
 
         Q = np.ones((self.S, self.A)) * 1/(1-gamma)  # Initialize state values
-        all_episodes = []
+        all_episodes_state_trajs = []
+        all_episodes_pos_trajs = []
         LL = 0.0
-        while len(all_episodes) < N_BOUTS_TO_GENERATE:
-            _, episodes, episode_ll = self.generate_exploration_episode(alpha, gamma, lamda, epsilon, k, n_plan,
-                                                                        np.zeros(Q.shape), np.zeros(Q.shape), MAX_LENGTH,
-                                                                        Q)  #TODO: missing parameter M
-            all_episodes.extend(episodes)
+        while len(all_episodes_state_trajs) < N_BOUTS_TO_GENERATE:
+            _, episode_state_trajs, episode_maze_trajs, episode_ll = self.generate_exploration_episode(alpha, gamma, lamda, epsilon, k, n_plan, bonus_in_planning, np.zeros(Q.shape), np.zeros(Q.shape), MAX_LENGTH, Q)
+            all_episodes_state_trajs.extend(episode_state_trajs)
+            all_episodes_pos_trajs.extend(episode_maze_trajs)
             LL += episode_ll
-        print("Q", Q)
-        print(all_episodes)
         stats = {
             "agentId": agentId,
-            "episodes": all_episodes,
+            "episodes_states": all_episodes_state_trajs,
+            "episodes_positions": all_episodes_pos_trajs,
             "LL": LL,
             "MAX_LENGTH": MAX_LENGTH,
-            "count_total": len(all_episodes),
+            "count_total": len(all_episodes_state_trajs),
             "Q": Q,
             "V": self.get_maze_state_values_from_action_values(Q),
-            "exploration_efficiency": em.exploration_efficiency(all_episodes, re=False),
-            "visit_frequency": calculate_visit_frequency(all_episodes)
+            "exploration_efficiency": em.exploration_efficiency(all_episodes_state_trajs, re=False),
+            "visit_frequency": calculate_visit_frequency(all_episodes_state_trajs)
         }
         return success, stats
 
